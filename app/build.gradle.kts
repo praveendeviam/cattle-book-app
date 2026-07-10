40d2223ccbe838e8b1cd94d9715dcbec9d26129c
Praveen <praveendeivam1817@gmail.com>
2026-07-10 19:48:16 +0530

    Bump versionCode to 3 for Play Store release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         applicationId = "io.github.praveendeviam.cattlebook"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
+        versionCode = 3
         versionName = "1.1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
